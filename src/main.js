e72560312233c5b040cde69f205f1004213a014f
Andor <andorhorvat@gmail.com>
2020-05-24 17:31:46 +0200

    17:31

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import ElementUI from 'element-ui';//needed for Element importing
+import 'element-ui/lib/theme-chalk/index.css';//needed for Element importing
+import App from './App.vue';
+import locale from 'element-ui/lib/locale/lang/en'//importing english into Elements
 
+
+Vue.use(ElementUI, { locale });//ElementUI is needed for Element importing, and with { locale } we import English language
 Vue.config.productionTip = false
 
 new Vue({
